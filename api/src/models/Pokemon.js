6d84b0830f1c4407478a2b0ddf4c402dfc309725
JhonDaniel01 <jhon-coso@hotmail.com>
2023-05-08 00:01:51 -0500

    Back 1.0

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,7 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    vida: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    ataque: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    defensa: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    velocidad: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    peso: {
       type: DataTypes.STRING,
       allowNull: false,
     },
